1d130614bb7e51a7f1bdb5fb59f6865d325440cd
Fernando del olmo martin <fer.olmo92@gmail.com>
2017-09-28 12:55:17 +0200

    feat(): Add scroll to last message

--- src/components/stencil-bot/stencil-bot.tsx
+++ src/components/stencil-bot/stencil-bot.tsx
@@ -23,15 +23,27 @@ export class StencilBot {
   }
 
   sendMessage() {
+    // Get input element
     let text = document.querySelector('input').value;
     document.querySelector('input').value = '';
+
+    // Add my message to the array
     this.messages = [];
     this.messages2.push({text: text, self: true});
     this.messages = this.messages2;
+
+    // Send the request to API.AI
     this.client.textRequest(text).then((response) => {
+
+      // Add the response message to the array
       this.messages = [];
       this.messages2.push({text: response.result.fulfillment.speech, self: false});
       this.messages = this.messages2;
+
+      // Scroll to bottom
+      let scroll = document.querySelector('ion-scroll');
+      let height = scroll.scrollHeight;
+      scroll.scrollTo(0, height);
     })
     .catch((error) => {
       console.log(error);
